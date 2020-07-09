83e4b6de1b631a71b7a61fefc87bfe826e22e254
Dante Marinho <dantecnia@hotmail.com>
2020-07-09 07:44:53 +0100

    Cleaning proj.

--- src/main/java/com/example/consumingrest/ScheduleClass.java
+++ src/main/java/com/example/consumingrest/ScheduleClass.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
 /**
- *
+ * Class created to send data scheduled.
  * @author HParaense
  */
 @Component
